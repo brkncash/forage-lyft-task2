c080502e36606a4689edb73f222838956dea515f
BrokenCashMachine <markiecash@outlook.com>
2023-09-28 20:21:54 +0100

    add battery package

---
+++ battery/nubbin_battery.py
@@ -0,0 +1,16 @@
+from abc import ABC
+
+from battery import Battery
+
+
+class NubbinBattery(Battery, ABC):
+
+    def __init__(self, last_service_date, current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def needs_service(self):
+        if (self.current_date - self.last_service_date).years < 4:
+            return False
+        else:
+            return True
